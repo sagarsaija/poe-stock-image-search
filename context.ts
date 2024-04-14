1829f6257605056a6f81c89db994828bf7aa683b
Aditya Advani <aditya@bestparents.com>
2024-04-14 12:44:15 -0700

    fix: mass cleanup

--- context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const AppContext = createContext({
-  model: {
-    value: "stable-diffusion",
-    label: "Stable Diffusion",
-  },
-  setModel: () => null,
-});
